c43709d1aaa35ef16657a3690ab5dcd4841b22a5
isXander <xander@isxander.dev>
2025-01-13 22:14:38 +0000

    Fix neoforge builds and builds less than 1.21.3 from loading

--- src/main/java/dev/isxander/controlify/ingame/InGameInputHandler.java
+++ src/main/java/dev/isxander/controlify/ingame/InGameInputHandler.java
@@ -30,7 +30,6 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.protocol.game.ServerboundPlayerActionPacket;
 import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.entity.player.Input;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.Vec3;
